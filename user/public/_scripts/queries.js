b10855ecf5f9781dca3cd23f652892013c9db14c
Michael Atterbury <aburymichael@users.noreply.github.com>
2022-09-01 22:41:08 +0200

    submit query

--- user/public/_scripts/queries.js
+++ user/public/_scripts/queries.js
@@ -1,4 +1,5 @@
-import { uploadFile } from './helpers.js'
+import { uploadFile, requestHandler } from './helpers.js'
+
 const submit = async function (e) {
   e.preventDefault()
   const form = e.currentTarget
@@ -11,31 +12,15 @@ const submit = async function (e) {
         body.delete(pair[0])
       }
     }
-
-    for (const pair of body.entries()) {
-      console.log(`${pair[0]}, ${pair[1]}`)
+  
+    const res = await requestHandler(form.action, form.method, body)
+    form.querySelector('button[type="submit"]').textContent = 'Submit query'
+    if (res.ok) {
+      form.reset()
+      document.querySelector('.modal').remove()
+    } else {
+      console.error(res)
     }
-
-              
-    // const res = await requestHandler(form.action, form.method, body)
-    // form.querySelector('button[type="submit"]').textContent = 'Submit query'
-    // if (res.ok) {
-    //   const link = JSON.parse(window.localStorage.getItem('link'))
-    //   logEvent(analytics, 'survey_complete', { 
-    //     name: `${link.package.name} - ${link.package.survey.title}`,
-    //     user_ref: window.localStorage.getItem('userRef'),
-    //     link_id: link.linkId,
-    //     package_id: link.package.docId,
-    //     time_stamp: dayjs().format('YYYY-MM-DD HH:mm:ssZ')
-    //   })
-    //   // const resBody = await res.json()
-    //   const localSubmissions = JSON.parse(window.localStorage.getItem('submissions')) || []
-    //   localSubmissions.push(body.get('linkId'))
-    //   window.location.href = `./complete?linkId=${link.linkId}`
-    // } else {
-    //   window.location.href = `./complete?linkId=${link.linkId}`
-    //   // throw new Error('An error occured whilst processing your request, please try again')
-    // }
   } catch (error) {
     form.querySelector('button[type="submit"]').textContent = 'Submit query'
     console.error(error)
