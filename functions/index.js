bacff95cdba723c409dd150d92d8ca66abaf1200
Michael Atterbury <aburymichael@users.noreply.github.com>
2022-09-04 23:14:53 +0200

    submit survey cloud function

--- functions/index.js
+++ functions/index.js
@@ -57,3 +57,27 @@ exports.deleteQuery = functions.https.onRequest(async (req, res) => {
     }
   }
 })
+
+exports.submitSurvey = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'POST')
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set('Access-Control-Max-Age', '3600')
+    res.status(204).send('')
+    res.end()
+  } else {
+    if (req.body.mineDocId && req.body.linkDocId) {
+      await admin.firestore().collection(`mines/${req.body.mineDocId}/links/${req.body.linkDocId}/responses`).add({
+        ...req.body,
+        createdAt: dayjs().format('YYYY-MM-DD HH:mm:ss')
+      })
+
+      res.send({ sucesss: true, message: 'Successfully submitted your responses' })
+      res.end()
+    } else {
+      res.status(412).send({ sucesss: false, message: 'Unable to submit response: We need a valid mine ID and link ID to continue' })
+      res.end()
+    }
+  }
+})
