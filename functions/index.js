febee8db7ed0899e44d88c2e5f40742a21ea663f
Michael Atterbury <aburymichael@users.noreply.github.com>
2022-09-15 23:57:13 +0200

    create user

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,54 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const dayjs = require('dayjs')
+const generator = require('generate-password')
 
-admin.initializeApp()
+if (admin.apps.length === 0) {
+  admin.initializeApp()
+}
+
+exports.createAuthentication = functions.firestore
+  .document('mines/{mineId}/users/{userId}')
+  .onCreate(async (snap, context) => {
+    const user = snap.data()
+    const password = generator.generate()
+
+    const userRecord = await admin.auth().createUser({
+      email: user.email,
+      phoneNumber: (user.mobile?.length) ? user.mobile : null,
+      displayName: user.name,
+      password
+    })
+
+    await admin.firestore().collection(`mines/${context.params.mineId}/users`).doc(context.params.userId).update({
+      authUid: userRecord.uid
+    })
+
+    await admin.firestore().collection("mail").add({
+      to: user.email,
+      message: {
+        subject: 'SATCAP Admin | New Account Created',
+        text: `Hi ${user.name}. Welcome to SATCAP Admin. Please sign in (https://admin-satcap-research.web.app) using the following password: ${password}`,
+        html: `
+          <div style="padding: 20px; background-color: rgb(247, 247, 247);border-radius:10px;max-width:640px;margin:0 auto;">
+            <img src="https://satcap-research.web.app/_style/images/emailheader.jpg" style="width:100%;object-fit:cover;max-height:150px;border-radius:10px;"/>
+            <h2 style="font-size: 2em;">Welcome ${user.name}</h2>
+            <br>
+            <p style="font-size: 1.2em;">You've just been added to the SATCAP Admin Platform; Happy to have you onboard!</p>
+            <p style="font-size: 1.2em;">Your Password is:  <strong style="font-size: 1.3em;">${password}</strong></p>
+            <br>
+            <p style="font-size: 1.2em;">Click below to get started:</p>
+            <a style="font-size: 1.2em;" href="https://admin-satcap-research.web.app">SATCAP Admin</a><br>
+            <br/><br/><br/>
+            <div style="text-align:center;width:100%;margin:0 auto;">
+              <img src="https://satcap-research.web.app/_style/images/logo.png" style="width:30px;" alt="SATCAP Logo" />
+              <strong style="text-align:center;font-size: 1em;">SATCAP</strong>
+            </div>
+          </div>
+        `
+      }
+    })
+  })
 
 exports.submitQuery = functions.https.onRequest(async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
