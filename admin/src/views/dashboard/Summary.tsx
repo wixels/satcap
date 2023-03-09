77ba4a8cafde11538da01be1c5c49333d111a12c
Dan Sivewright <dan@wixels.com>
2023-03-09 08:35:09 +0200

    update alt eco table desc

--- admin/src/views/dashboard/Summary.tsx
+++ admin/src/views/dashboard/Summary.tsx
@@ -154,7 +154,7 @@ const tableData = {
     {
       title: 'Alternative Economy Industries',
       description:
-        'Green – 66% or more responders picked the following skill. Yellow – Between 33% and 65% responders picked the following skill. Red – Less than 33% picked the following skill. ',
+        'Red – 66% or more responders picked the following skill. Yellow – Between 33% and 65% responders picked the following skill. Green – Less than 33% picked the following skill. ',
       levels: {
         above: 66,
         below: 33,
