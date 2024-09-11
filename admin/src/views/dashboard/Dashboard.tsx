45ff5142be99df56d3bd561e096aa0493acebec3
Dan Sivewright <dan@wixels.com>
2024-09-11 10:40:04 +0200

    add video to dashboard

--- admin/src/views/dashboard/Dashboard.tsx
+++ admin/src/views/dashboard/Dashboard.tsx
@@ -1,16 +1,29 @@
-import { Card, SimpleGrid, Text } from '@mantine/core';
+import { AspectRatio, Card, SimpleGrid, Text } from '@mantine/core';
+import { Notification } from '@mantine/core';
+
 import { useLocalStorage } from '@mantine/hooks';
 import { useEffect } from 'react';
 import { useGetMine } from '../../hooks/network/useMine';
 
 export const Dashboard = () => {
-
   // DATA
   const { data: mine, isLoading } = useGetMine();
 
   return (
     <>
-      <SimpleGrid
+      <Notification disallowClose mb="xl" title="The dashboard has moved">
+        Please view the dashboard on Power BI
+      </Notification>
+      <AspectRatio ratio={1920 / 1080}>
+        <video
+          src="https://mandelaminingprecinct.org.za/wp-content/uploads/2024/06/ESG-Impacts-Lens-Dashboard-Overview.mp4"
+          controls
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        >
+          Your browser does not support the video tag.
+        </video>
+      </AspectRatio>
+      {/* <SimpleGrid
         cols={3}
         spacing="lg"
         breakpoints={[
@@ -33,7 +46,7 @@ export const Dashboard = () => {
             </Card.Section>
           </Card>
         ))}
-      </SimpleGrid>
+      </SimpleGrid> */}
     </>
   );
 };
